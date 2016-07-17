79160552bfa782ed2f7f0a207d2414d64d8243de
tmurray <tmurray@wumpus-cave.net>
2016-07-16 21:01:48 -0500

    Basic SQL statement

--- lib/SQL/Functional.pm
+++ lib/SQL/Functional.pm
@@ -2,9 +2,35 @@ package SQL::Functional;
 
 use v5.14;
 use warnings;
+use Exporter;
+our @ISA = qw{ Exporter };
+
+our @EXPORT_OK = qw{
+    SELECT
+    star
+    FROM
+};
+our @EXPORT = @EXPORT_OK;
 
 # ABSTRACT: Create SQL through abstract functions
 
+
+sub SELECT (@)
+{
+    my (@substrs) = @_;
+    return join ' ', 'SELECT', @substrs;
+}
+
+sub star ()
+{
+    return '*';
+}
+
+sub FROM (@)
+{
+    my (@list) = @_;
+    return( 'FROM', join( ', ', @list ) );
+}
 
 1;
 __END__
